fc6c227aef4c92791f8862a24e8a1d10ff3e831c
Alfan Rozak <93852663+alfanzaky@users.noreply.github.com>
2025-06-29 16:23:20 +0700

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,34 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const currentPage = window.location.pathname.split('/').pop();
-  const navMenu = document.getElementById('nav-menu');
-  const isLoggedIn = localStorage.getItem("userLoggedIn") === "true"; // opsional
+const currentPage = window.location.pathname.split('/').pop();
+const navMenu = document.getElementById('nav-menu');
+const isLoggedIn = localStorage.getItem("userLoggedIn") === "true";
 
-  if (navMenu) {
-    // Tampilkan "Beranda" kalau user tidak di index.html
-    if (currentPage !== "index.html") {
-      navMenu.innerHTML += `
-        <li class="nav-item">
-          <a class="nav-link" href="/app-vehicle-monitoring/index.html">Beranda</a>
-        </li>`;
-    }
-
-    // Tambah menu "Tentang"
+if (navMenu) {
+  if (currentPage !== "index.html") {
     navMenu.innerHTML += `
       <li class="nav-item">
-        <a class="nav-link" href="/app-vehicle-monitoring/pages/tentang.html">Tentang</a>
+        <a class="nav-link" href="/app-vehicle-monitoring/index.html">Beranda</a>
       </li>`;
-
-    // Login atau Profil (opsional, bisa diaktifkan nanti)
-    if (isLoggedIn) {
-      navMenu.innerHTML += `
-        <li class="nav-item">
-          <a class="nav-link" href="/app-vehicle-monitoring/pages/profil.html">Profil</a>
-        </li>`;
-    } else {
-      navMenu.innerHTML += `
-        <li class="nav-item">
-          <a class="nav-link" href="/app-vehicle-monitoring/pages/login.html">Login</a>
-        </li>`;
-    }
   }
-});
+
+  navMenu.innerHTML += `
+    <li class="nav-item">
+      <a class="nav-link" href="/app-vehicle-monitoring/pages/tentang.html">Tentang</a>
+    </li>`;
+
+  if (isLoggedIn) {
+    navMenu.innerHTML += `
+      <li class="nav-item">
+        <a class="nav-link" href="/app-vehicle-monitoring/pages/profil.html">Profil</a>
+      </li>`;
+  } else {
+    navMenu.innerHTML += `
+      <li class="nav-item">
+        <a class="nav-link" href="/app-vehicle-monitoring/pages/login.html">Login</a>
+      </li>`;
+  }
+}
